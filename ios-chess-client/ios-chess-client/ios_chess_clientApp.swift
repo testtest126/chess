dc57f75c740eafc4fc693ff9a93c44e54047d662
Yakiv Kovalskyi <ykovalsky.up@gmail.com>
2026-07-09 16:26:09 +0200

    init

---
+++ ios-chess-client/ios-chess-client/ios_chess_clientApp.swift
@@ -0,0 +1,32 @@
+//
+//  ios_chess_clientApp.swift
+//  ios-chess-client
+//
+//  Created by Y K on 09.07.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ios_chess_clientApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ios-chess-client/ios-chess-clientTests/ios_chess_clientTests.swift
@@ -0,0 +1,19 @@
+//
+//  ios_chess_clientTests.swift
+//  ios-chess-clientTests
+//
+//  Created by Y K on 09.07.26.
+//
+
+import Testing
+@testable import ios_chess_client
+
+struct ios_chess_clientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
